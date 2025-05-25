321816f6f3bde7eba727ccf1ea35124d679be18c
Malik Majeed <malikmajeedzia@gmail.com>
2025-05-25 20:49:38 +0500

    authentication issue resolved and routes tested

--- Backend/Routes/users.router.js
+++ Backend/Routes/users.router.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { signUp, login, getUser, getAllDonors,deleteUser, updateUser } from "../Controllers/users.controller.js";
-
+import { signUp, login, getUser, getAllDonors, deleteUser, updateUser } from "../Controllers/users.controller.js";
+import { authenticateToken as Auth } from "../Middlewares/authentication.js";
 const router = express.Router();
 
 
@@ -11,17 +11,17 @@ router.post("/signup", signUp); //✅ verified
 router.post("/login", login); //✅ verified
 
 //get user route
-router.get("/:id", getUser); //✅ verified
-
+router.get("/:id", Auth, getUser); //✅ verified
+users98765432
 //get all donors route
 // when no donors exists it takes lot of time to complete the request
-router.get("/donors", getAllDonors);//✅ verified
+router.get("/donors/abc", Auth, getAllDonors);//✅ verified
 
 //update user route
-router.patch("/update", updateUser);//✅ verified
+router.patch("/update", Auth, updateUser);//✅ verified
 
 //deleting user route
-router.delete("/delete/:id", deleteUser)//✅ verified
+router.delete("/delete/:id", Auth, deleteUser)//✅ verified
 
 
 export default router;
