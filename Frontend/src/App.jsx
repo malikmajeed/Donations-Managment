c278e1999dc110429835eb3daaad0e0a2991c8b4
Malik Majeed <malikmajeedzia@gmail.com>
2025-07-01 14:31:12 +0500

    Collapse menu updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,45 +1,14 @@
-import { useState } from 'react'
-import HeroSection from './pages/Home/HeroSection.jsx'
-import SignUp from './components/signUp'
-import './App.css'
-import ErrorBoundary from './components/ErrorBoundary.jsx' 
-import SignIn from './components/signIn'
-import AddStudent from './components/addStudent'
-import GetStudentByID from './components/getStudent'
-import GetAllStudents from './components/getAllStudents'
-import UpdateAndDeleteStudent from './components/updateStudent'
-import UserDashboard from './components/userDashboard/index.jsx'
-import UpdateProfile from './components/updateProfile'
-import ForgetPassword from './components/forgetPassword'
+import MenuHeader from './components/header/MenuHeader' 
+
+
 
 function App() {
-  
- 
 
   return (
     <>
- 
-    
-     <SignUp />  
-           
-      <SignIn/>              
-      <ForgetPassword />
+      <MenuHeader />
      
-    
-      <GetAllStudents />
-      <GetStudentByID studentId="6842cacb05709a76c110f023" />
-    <UpdateAndDeleteStudent studentId="6842cacb05709a76c110f023"  />
-
-  
-    
-   
-    <AddStudent />
-    <ErrorBoundary >
-    <UserDashboard />
-    </ErrorBoundary>
-   
-    <UpdateProfile userId="683af0de9c826d38dca8fb34"/>
-    </>
+     </>
   )
 }
 
