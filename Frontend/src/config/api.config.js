4997ecee68571cdbcb1bd85a8c95a3f5f7cf9ea2
Malik Majeed <malikmajeedzia@gmail.com>
2025-07-04 23:35:33 +0500

    Api_config file updated for all routes

--- Frontend/src/config/api.config.js
+++ Frontend/src/config/api.config.js
@@ -15,7 +15,34 @@ export const API_CONFIG = {
         },
         DONATIONS: {
             LIST: `${API_BASE_URL}/donation`,
-            CREATE: `${API_BASE_URL}/donation/create`,
+            CREATE: `${API_BASE_URL}/donation/add`,
+            STATISTICS: `${API_BASE_URL}/donation/statistics`,
+            BY_ID: id => `${API_BASE_URL}/donation/${id}`,
+            BY_TARGET: (type, id) => `${API_BASE_URL}/donation/target/${type}/${id}`,
+            UPDATE_STATUS: id => `${API_BASE_URL}/donation/${id}/status`,
+            DELETE: id => `${API_BASE_URL}/donation/${id}`
+        },
+        CAUSES: {
+            LIST: `${API_BASE_URL}/causes/getAllCauses`,
+            CREATE: `${API_BASE_URL}/causes/createCause`,
+            BY_ID: id => `${API_BASE_URL}/causes/getCauseById/${id}`,
+            UPDATE: id => `${API_BASE_URL}/causes/updateCause/${id}`,
+            DELETE: id => `${API_BASE_URL}/causes/deleteCause/${id}`,
+            BY_TYPE: type => `${API_BASE_URL}/causes/getCausesByType/${type}`,
+            URGENT: `${API_BASE_URL}/causes/getUrgentCauses`,
+            STATISTICS: `${API_BASE_URL}/causes/getStatistics`,
+            UPDATE_AMOUNT: id => `${API_BASE_URL}/causes/updateAmountCollected/${id}`
+        },
+        STUDENTS: {
+            LIST: `${API_BASE_URL}/student/getAllStudents`,
+            CREATE: `${API_BASE_URL}/student/addStudent`,
+            BY_ID: id => `${API_BASE_URL}/student/getStudentbyId/${id}`,
+            UPDATE: id => `${API_BASE_URL}/student/updateStudent/${id}`,
+            DELETE: id => `${API_BASE_URL}/student/deleteStudent/${id}`,
+            UPDATE_SPONSORSHIP: id => `${API_BASE_URL}/student/updateSponsorship/${id}`,
+            UPDATE_FEE_STATUS: id => `${API_BASE_URL}/student/updateFeeStatus/${id}`,
+            RECORD_PAYMENT: id => `${API_BASE_URL}/student/recordPayment/${id}`,
+            FEE_SUMMARY: id => `${API_BASE_URL}/student/getFeeSummary/${id}`
         }
     }
 };
